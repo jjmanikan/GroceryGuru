384a67ebe5f3890747c9f59039ae824dcbcdc29c
Jake Hazzard <jake.hazzard@outlook.com>
2018-01-04 14:17:43 -0500

    Added add_page.xml and AddList.java for adding new items to Listview from database

---
+++ app/src/main/java/com/example/justine/groceryguru/AddList.java
@@ -0,0 +1,20 @@
+package com.example.justine.groceryguru;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Asus on 2018-01-04.
+ */
+
+public class AddList extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.add_page);
+
+
+    }
+
+}
--- app/src/main/java/com/example/justine/groceryguru/MainActivity.java
+++ app/src/main/java/com/example/justine/groceryguru/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.justine.groceryguru;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
 
 public class MainActivity extends Activity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //setContentView(R.layout.add_page);
+
+
+
     }
 }
