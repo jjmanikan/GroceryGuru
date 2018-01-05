4a7a2b0496ccbaed96793883e9df4f73402204ef
Jake Hazzard <jake.hazzard@outlook.com>
2018-01-05 13:11:29 -0500

    Added add_page.xml and AddList.java for adding new items to Listview from database Added view_list.xml and ViewListItem.java for adding individual items to lists Added navigation functionality between activities and intents for results Changed get to receive default items from JSON hosted on http://hazzarja.dev.fast.sheridanc.on.ca/list_data.json

--- app/src/main/java/com/example/justine/groceryguru/MainActivity.java
+++ app/src/main/java/com/example/justine/groceryguru/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.justine.groceryguru;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
@@ -10,17 +13,40 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
+
 import java.util.ArrayList;
 
 public class MainActivity extends Activity {
 
     Button addList;
     ListView list;
-    ArrayList<String> listItems;
+    ListView subList;
+    //ArrayList<String> listItems;
+    ArrayList<ArrayList<String>> dataMatrix;
+    ArrayList<String> row;
+    ArrayList<String> titles;
+    //ArrayList<String> subListItems;
     ArrayAdapter<String> adapter;
+    //ArrayAdapter<String> subAdapter;
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+    String urlTxt;
     private static final int DETAIL_ACTIVITY = 2;
     private static final int VIEW_ACTIVITY = 3;
     private int pos;
+
 
     public void setPos(int num){this.pos = num;}
 
@@ -31,8 +57,20 @@ public class MainActivity extends Activity {
 
         addList = findViewById(R.id._btnAdd);
         list = findViewById(R.id.listView);
+        subList = findViewById(R.id.listView2);
 
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        urlTxt = "http://hazzarja.dev.fast.sheridanc.on.ca/list_data.json";
+
+        fragmentManager = getFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+
+        row = new ArrayList<>();
+        titles = new ArrayList<>();
+        //subListItems = new ArrayList<>
+        autoPopulate();
+
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, titles);
         list.setAdapter(adapter);
 
         setPos(0);
@@ -48,24 +86,36 @@ public class MainActivity extends Activity {
         list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(
+                /*Intent intent = new Intent(
                         getApplicationContext(),
                         ViewListItem.class
-                );
+                );*/
+                Intent intent = new Intent();
+                intent.setClass(getApplicationContext(), ViewListItem.class);
+                intent.putExtra("position", position);
 
-                startActivityForResult(intent, VIEW_ACTIVITY);
+                startActivity(intent);
+
+                //startActivity(intent);
+                //startActivityForResult(intent, VIEW_ACTIVITY);
             }
         });
+
 
 
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data){
+
+        String title = data.getStringExtra("title");
+        titles.add(title);
+
         if (requestCode == DETAIL_ACTIVITY) {
             if (resultCode == Activity.RESULT_OK) {
 
-                String title = data.getStringExtra("title");
+                //String title = data.getStringExtra("title");
+                //titles.add(title);
 
             }
         }
@@ -75,5 +125,120 @@ public class MainActivity extends Activity {
 
             }
         }
+    }
+
+    public void autoPopulate(){
+
+        /*ViewListItem pg0;
+        ViewListItem pg1;
+        ViewListItem pg2;
+        ViewListItem pg3;
+        ViewListItem pg4;
+        ViewListItem pg5;
+        ViewListItem pg6;
+        ViewListItem pg7;
+        ViewListItem pg8;
+        ViewListItem pg9;*/
+
+        titles.add("Groceries");
+        titles.add("Clothes");
+        titles.add("Electronics");
+        /*titles.add("");
+        titles.add("");
+        titles.add("");
+        titles.add("");
+        titles.add("");
+        titles.add("");
+        titles.add("");*/
+    }
+
+    public void loadWebJson(View view){
+
+        ProcessTask process = new ProcessTask();
+        process.execute(urlTxt);
+
+    }
+
+    private class ProcessTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... strings) {
+
+            try {
+
+                URL url = new URL(strings[0]);
+                URLConnection connect = url.openConnection();
+                HttpURLConnection httpConnect = (HttpURLConnection) connect;
+
+                int responseCode = httpConnect.getResponseCode();
+                if (responseCode == HttpURLConnection.HTTP_OK) {
+
+                    InputStream is = httpConnect.getInputStream();
+                    Scanner scanner = new Scanner(is);
+                    StringBuilder builder = new StringBuilder();
+
+                    while (scanner.hasNextLine()) {
+                        builder.append(scanner.nextLine());
+                    }
+
+                    return parseJsonFile(builder.toString());
+
+                } else {
+                    return "Http Error";
+                }
+            } catch (Exception e) {
+                return e.toString();
+            }
+        }
+
+        private String parseJsonFile(String s) {
+
+            //StringBuilder builder = new StringBuilder();
+
+            try {
+                JSONObject wrapper = new JSONObject(s);
+                JSONObject listData = wrapper.getJSONObject("list-data");
+
+                JSONArray groceries = listData.getJSONArray("groceries");
+                JSONArray clothes = listData.getJSONArray("clothes");
+                JSONArray electronics = listData.getJSONArray("electronics");
+
+
+                for (int i = 0; i < groceries.length(); i++) {
+                    JSONObject defaultOne = groceries.getJSONObject(i);
+                    //builder.append(defaultOne.getString("name")).append(": ").append(defaultOne.getString("number")).append("\n");
+                    row.add(defaultOne.getString("name"));
+                }
+                dataMatrix.add(row);
+                row.clear();
+                for (int i = 0; i < clothes.length(); i++) {
+                    JSONObject defaultTwo = clothes.getJSONObject(i);
+                    //builder.append(defaultOne.getString("name")).append(": ").append(defaultOne.getString("number")).append("\n");
+                    row.add(defaultTwo.getString("name"));
+                }
+                dataMatrix.add(row);
+                row.clear();
+                for (int i = 0; i < electronics.length(); i++) {
+                    JSONObject defaultThree = electronics.getJSONObject(i);
+                    //builder.append(defaultOne.getString("name")).append(": ").append(defaultOne.getString("number")).append("\n");
+                    row.add(defaultThree.getString("name"));
+                }
+                dataMatrix.add(row);
+                row.clear();
+
+                //return builder.toString();
+            } catch (JSONException e) {
+                e.printStackTrace();
+                return null;
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s){
+
+            //dataTxt.setText(s);
+
+        }
+
     }
 }
