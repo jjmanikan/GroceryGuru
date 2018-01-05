7989a5b09ebae1b41a9d6cecc39236c92eb428c2
Jake Hazzard <jake.hazzard@outlook.com>
2018-01-04 19:34:08 -0500

    Added add_page.xml and AddList.java for adding new items to Listview from database Added view_list.xml and ViewListItem.java for adding individual items to lists

--- app/src/main/java/com/example/justine/groceryguru/AddList.java
+++ app/src/main/java/com/example/justine/groceryguru/AddList.java
@@ -1,7 +1,13 @@
 package com.example.justine.groceryguru;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 /**
  * Created by Asus on 2018-01-04.
@@ -9,11 +15,41 @@ import android.os.Bundle;
 
 public class AddList extends Activity {
 
+    /*private SQLiteDatabase database;
+    private SQLiteOpenHelper openHelper;
+
+    private static final String DB_NAME="lists.db";
+    private static final int DB_VERSION = 1;
+
+    private static final String LISTS_TABLE = "Lists";
+
+    private static final String  = "_id";
+    private static final int ID_COLUMN = 0;*/
+
+    EditText listname;
+    Button addEntry;
+    Button previous;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_page);
 
+        listname = findViewById(R.id._txtName);
+        addEntry = findViewById(R.id._btnAddEntry);
+        previous = findViewById(R.id._btnViewLists);
+
+        addEntry.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = listname.getText().toString();
+                Intent data = new Intent();
+                data.putExtra("title", name);
+                setResult(Activity.RESULT_OK, data);
+                finish();
+            }
+        });
 
     }
 
--- app/src/main/java/com/example/justine/groceryguru/MainActivity.java
+++ app/src/main/java/com/example/justine/groceryguru/MainActivity.java
@@ -1,21 +1,79 @@
 package com.example.justine.groceryguru;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
+import java.util.ArrayList;
+
 public class MainActivity extends Activity {
 
+    Button addList;
+    ListView list;
+    ArrayList<String> listItems;
+    ArrayAdapter<String> adapter;
+    private static final int DETAIL_ACTIVITY = 2;
+    private static final int VIEW_ACTIVITY = 3;
+    private int pos;
 
+    public void setPos(int num){this.pos = num;}
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //setContentView(R.layout.add_page);
+
+        addList = findViewById(R.id._btnAdd);
+        list = findViewById(R.id.listView);
+
+        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1);
+        list.setAdapter(adapter);
+
+        setPos(0);
+
+        addList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AddList.class);
+                startActivityForResult(intent, DETAIL_ACTIVITY);
+            }
+        });
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent(
+                        getApplicationContext(),
+                        ViewListItem.class
+                );
+
+                startActivityForResult(intent, VIEW_ACTIVITY);
+            }
+        });
 
 
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        if (requestCode == DETAIL_ACTIVITY) {
+            if (resultCode == Activity.RESULT_OK) {
+
+                String title = data.getStringExtra("title");
+
+            }
+        }
+
+        if (requestCode == VIEW_ACTIVITY){
+            if (requestCode == Activity.RESULT_OK){
+
+            }
+        }
     }
 }
